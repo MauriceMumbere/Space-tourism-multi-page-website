8bce8fbad0997e96e38a94dbdaa6a750d34925ec
maurice Mumbere <mauricendayango@gmail.com>
2025-12-28 15:27:40 +0100

    feat : create index page

---
+++ app/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section
+    class="h-screen bg-[url('/home/background-home-mobile.jpg')] md:bg-[url('/home/background-home-tablet.jpg')] lg:bg-[url('/home/background-home-desktop.jpg')] bg-no-repeat bg-cover flex items-center md:items-end"
+  >
+    <div
+      class="text-white w-[95%] md:w-[75%] mx-auto flex flex-col lg:flex-row lg:items-end lg:py-24 mt-10 md:mt-0"
+    >
+      <div class="space-y-5 lg:text-left text-center">
+        <h2>SO, YOU WANT TO TRAVEL TO</h2>
+        <h1 class="text-8xl md:text-9xl">SPACE</h1>
+        <p class="lg:max-w-[62%] leading-8 m-auto lg:m-0">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+
+      <div class="flex justify-center items-center mt-15 md:mt-7">
+        <div
+          class="flex justify-center items-center w-36 h-36 md:w-[272px] md:h-[272px] bg-white text-black rounded-full"
+        >
+          EXPLORE
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
